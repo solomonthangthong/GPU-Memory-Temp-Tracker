b65f8e45f48da26598277f9e8be86e75c82fc650
Solomon Thangthong <than0047@algonquinlive.com>
2025-02-06 18:16:52 -0500

    Init program.cs

---
+++ MemTempTracker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+namespace MemTempTracker
+{
+    internal class Program
+    {
+        // Hardcoded path, update to your log file location
+        static string logFilePath = @"C:\Users\mon\Documents\GPUZ\Log.txt";
+        static string targetProcess = "Salad";
+        static char delimiter = ',';
+        static int tempThreshold = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitoring GPU-Z logs for VRAM temperature...");
+        }
+    }
+}
