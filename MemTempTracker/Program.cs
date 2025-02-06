6050618b125f60882bf81af6555d9c6ed9b7e555
Solomon Thangthong <than0047@algonquinlive.com>
2025-02-06 18:39:14 -0500

    Program start

--- MemTempTracker/Program.cs
+++ MemTempTracker/Program.cs
@@ -15,9 +15,65 @@ namespace MemTempTracker
         static string targetProcess = "Salad";
         static char delimiter = ',';
         static int tempThreshold = 100;
+
+
+        static double? GetLastestMemoryTemperature() 
+        {
+            if (!File.Exists(logFilePath)) {
+                Console.WriteLine("GPU-Z log file not found.");
+                return null;
+            }
+
+            try {
+                // Latest reading is at the bottom, so read from reverse
+                var lines = File.ReadLines(logFilePath).Reverse().ToList();
+                if (lines.Count < 2) return null;
+
+                //Retrieve first line in the file, split by comma, h => h remove extra spaces, and return into Array
+                // Date, GPU Clock [MHz], Memory Temperature [°C], Fan Speed [%] to ["Date", "GPU Clock [MHz]", "Memory Temperature [°C]", "Fan Speed [%]"]
+                var header = lines.Last().Split(',').Select(h => h.Trim()).ToArray();
+
+                // Get last line in file, split into array by comma, remove extra spaces
+                // 2025-02-06 18:04:10, 210.0, 36.0, 20 to ["2025-02-06 18:04:10", "210.0", "36.0", "20"]
+                var latestData = lines.First().Split(',').Select(d => d.Trim()).ToArray(); // Latest entry
+
+                int memoryTempIndex = Array.FindIndex(header, col => col.Equals("Memory Temperature [°C]", StringComparison.OrdinalIgnoreCase));
+
+                if (memoryTempIndex == -1) 
+                {
+                    Console.WriteLine("Memory Temperature Column not found");
+                    return null;
+                }
+
+                // out double temp store the parsed number when TryParse successfully converts string into double
+                if (double.TryParse(latestData[memoryTempIndex], out double temp)) 
+                { 
+                    return temp; 
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading log: {ex.Message}");
+            }
+
+            return null;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Monitoring GPU-Z logs for VRAM temperature...");
+
+            while (true) {
+                try
+                {
+                    double? latestTemp = GetLastestMemoryTemperature();
+                }
+                catch (Exception ex) {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                Thread.Sleep(5000); // Check every 5 seconds
+            }
         }
     }
 }
